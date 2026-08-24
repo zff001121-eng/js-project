6c81168cfa8a6d757ff56340c5145b8a39e989a1
zff001121-eng <zff001121@gmail.com>
2026-08-24 11:44:19 +0800

    Mock browser environment in pdd_env.js
    Added mock implementations for various browser APIs and objects.

--- pdd_env.js
+++ pdd_env.js
@@ -1,1 +1,44 @@
 
+window = global
+// delete Buffer
+document = {
+    addEventListener: function(){},
+    cookie:""
+}
+screen  = {
+    availWidth: 1920,
+    availHeight: 1032
+}
+localStorage = {
+    _nano_fp :"Xpm8n5E8XpdyX0dyno_f8uaWG~bhKpkANqy0eMPf",
+}
+window.chrome = {}
+window.DeviceOrientationEvent = function (){}
+window.DeviceMotionEvent = function (){}
+window.Buffer = undefined
+window.outerHeight = 1032
+window.outerWidth = 1920
+history = {}
+location = {
+    "ancestorOrigins": {},
+    "href": "https://pinduoduo.com/home/girlclothes/",
+    "origin": "https://pinduoduo.com",
+    "protocol": "https:",
+    "host": "pinduoduo.com",
+    "hostname": "pinduoduo.com",
+    "port": "",
+    "pathname": "/home/girlclothes/",
+    "search": "",
+    "hash": ""
+}
+Navigator = {
+    webdriver:false,
+    plugins:{
+        length:5
+    },
+    languages: ['zh-CN', 'en', 'en-GB', 'en-US'],
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36 Edg/146.0.0.0"
+}
+
+navigator = {}
+Object.setPrototypeOf(navigator, Navigator)
